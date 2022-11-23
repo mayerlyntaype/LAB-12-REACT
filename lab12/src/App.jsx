9cd68ad8dcfc4d4e9d638ac53e1aae5e8b307419
Mayerlyn Taype <mayerlyn.taype@tecsup.edu.pe>
2022-11-23 17:49:45 -0500

    lab terminado

---
+++ lab12/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PosPos from './modulos/pos/pages/PosPos';
+
+const App = () => {
+	return (
+		<h1><PosPos/></h1>
+	)
+}
+
+export default App;
---
+++ lab12/src/modulos/pos/components/PosBoleta.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { PosBoletaItem } from './PosBoletaItem';
+
+const PosBoleta = ({objMesa}) => {
+	
+	const pedido = {
+		pedidoPlatos : [
+			{
+				id: 1,
+				nombre: "CAUSA LIMEÑA",
+				cantidad:1,
+				precio: 10.00
+			},
+			{
+				id: 2,
+				nombre: "PAPA RELLENA",
+				cantidad:2,
+				precio: 15.00
+			}
+		]
+	}
+
+	return (
+		<div className="boleta">
+			<h3>
+				Pedido Mesa: &nbsp;
+				<span className="color-secundario">
+					{objMesa ? objMesa.nro : 'seleccione'}
+				</span>
+			</h3>
+			<hr />
+			<div className="comanda">
+				<h4 className="comanda__mesa">
+					{objMesa ? `Mesa ${objMesa.nro}` : 'seleccione'}
+				</h4>
+				<p className="comanda__usuario">Mayerlyn Taype</p>
+				<hr />
+
+				<ul className="comanda__lista">
+					{
+					pedido.pedidoPlatos.map((objPlatoPedido, i) => {
+								return (
+									<PosBoletaItem objPlatoPedido={objPlatoPedido} key={i} />
+								);
+						  })
+					}
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default PosBoleta;
